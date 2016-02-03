fb74a883752bbdf4343972e45894376495a793bc
Tom Chen <developer@tomchentw.com>
2016-02-04 00:36:14 +0800

    test(webpack): fix webpackExternalsResolver test cases

--- src/webpack/__tests__/createWebpackConfig.spec.js
+++ src/webpack/__tests__/createWebpackConfig.spec.js
@@ -5,29 +5,37 @@ import {
 } from "expect";
 
 import {
-  webpackExternalsResolver,
+  default as thenify,
+} from "thenify";
+
+import {
+  webpackExternalsResolver as nodeWebpackExternalsResolver,
 } from "../createWebpackConfig";
+
+const webpackExternalsResolver = thenify(nodeWebpackExternalsResolver);
 
 const context = `/some/context/path`;
 
 describe(`createWebpackConfig`, function describeCreateWebpackConfig() {
   describe(`webpackExternalsResolver`, function describeWebpackExternalsResolver() {
-    it(`transform node built-in module`, function it() {
-      webpackExternalsResolver(context, `path`, (error, result) => {
-        expect(result).toEqual(`commonjs path`);
-      });
+    it(`transform node built-in module`, async function it() {
+      const result = await webpackExternalsResolver(context, `path`);
+      expect(result).toEqual(`path`);
     });
 
-    it(`transform local modules in node_modules`, function it() {
-      webpackExternalsResolver(context, `lodash/fp/map`, (error, result) => {
-        expect(result).toEqual(`commonjs lodash/fp/map`);
-      });
+    it(`transform local modules in node_modules`, async function it() {
+      const result = await webpackExternalsResolver(context, `lodash/fp/map`);
+      expect(result).toInclude(`reacthtmlpack/node_modules/lodash/fp/map.js`);
     });
 
-    it(`transform local modules with webpack loader syntax`, function it() {
-      webpackExternalsResolver(context, `css?module!flexboxgrid`, (error, result) => {
-        expect(result).toEqual(`commonjs flexboxgrid`);
-      });
+    it(`transform local modules with webpack loader syntax`, async function it() {
+      const result = await webpackExternalsResolver(context, `css?module!lodash`);
+      expect(result).toInclude(`reacthtmlpack/node_modules/lodash/lodash.js`);
+    });
+
+    it(`transform local modules with css-modules`, async function it() {
+      const result = await webpackExternalsResolver(context, `css?module!normalize.css`);
+      expect(result).toBeFalsy();
     });
   });
 });
